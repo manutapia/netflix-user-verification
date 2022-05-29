a612bc6fd79d5097b319eda6f8b0b5010006754b
Manuel Tapia <manutapia@barracks.gg>
2022-05-29 01:52:46 -0400

    add config-env to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { VerificationModule } from './modules/verification/verification.module';
+import { ConfigEnvModule } from './config-env/config-env.module';
 
 @Module({
-  imports: [VerificationModule],
+  imports: [VerificationModule, ConfigEnvModule],
   controllers: [AppController],
   providers: [AppService],
 })
