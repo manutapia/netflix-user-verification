2119036f5e481cb91e9f14dc3e22ba056bc1824e
Manuel Tapia <manutapia@barracks.gg>
2022-08-13 16:08:08 -0400

    remove date limit and update styles

--- src/modules/verification/verification.service.ts
+++ src/modules/verification/verification.service.ts
@@ -16,7 +16,7 @@ export class VerificationService {
         const lastweek = new Date();
         lastweek.setDate(lastweek.getDate() - 1);
         const around = `"Perfil AROUND ${accountName}"`
-        const messages = await this._gmail.getMessageFrom(from, subject, lastweek, around);
+        const messages = await this._gmail.getMessageFrom(from, subject, null, around);
         if (!messages) {
             return []
         }
@@ -25,7 +25,7 @@ export class VerificationService {
         const userVerificationMessages = messagesContent.map((message: any) => {
             const encodedMessage = message.payload["parts"][0].body.data;
             const dateaux = new Date(message.payload["headers"][3].value.split(";")[1])
-            dateaux.setHours(dateaux.getHours() - 4)
+            // dateaux.setHours(dateaux.getHours() - 4)
             const fulldate = dateaux.toLocaleDateString('es-CL');
             const fulltime = dateaux.toLocaleTimeString('es-CL');
             const decodedStr = Buffer.from(encodedMessage, "base64").toString(
